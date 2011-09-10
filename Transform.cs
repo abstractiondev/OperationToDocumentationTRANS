7ed97a604cbeec9deabdad07592f3ab3bd334bf7
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-10 12:26:11 +0300

    Cleaned up configuration to absconfig

--- Transform.cs
+++ Transform.cs
@@ -9,14 +9,14 @@ namespace OperationToDocumentationTRANS
 {
     public class Transform
     {
-        public DocumentationAbstractionType TransformAbstraction(OperationAbstractionType operationAbstraction)
+        public static DocumentationAbstractionType TransformAbstraction(OperationAbstractionType operationAbstraction)
         {
             DocumentType document = GetDocument(operationAbstraction);
-            return new DocumentationAbstractionType()
+            return new DocumentationAbstractionType
                        {
-                           Documentations = new DocumentationsType()
+                           Documentations = new DocumentationsType
                                                 {
-                                                    Documents = new DocumentType[]
+                                                    Documents = new[]
                                                                     {
                                                                         GetDocument(operationAbstraction)
 
@@ -27,49 +27,45 @@ namespace OperationToDocumentationTRANS
 
         private static DocumentType GetDocument(OperationAbstractionType operationAbstraction)
         {
-            DocumentType document = new DocumentType();
-            int stackLevel = 1;
-            document.Content = operationAbstraction.Operations.Operation.Select(
-                operation => GetOperationContent(operation, stackLevel)).ToArray();
+            DocumentType document = new DocumentType
+                                        {
+                                            Content = operationAbstraction.Operations.Operation.Select(
+                                                operation => GetOperationContent(operation)).ToArray()
+                                        };
             return document;
         }
 
-        private static HeaderType GetOperationContent(OperationType operation, int stackLevel)
+        private static HeaderType GetOperationContent(OperationType operation)
         {
             HeaderType header = new HeaderType
                                     {
                                         text = operation.name,
-                                        level = stackLevel,
+                                        level = 1,
                                     };
             List<HeaderType> subHeaders = new List<HeaderType>();
             subHeaders.AddRange(
-                operation.Parameters.Parameter.Select(
-                    param => GetParameterContent(param, stackLevel + 1)));
+                operation.Parameters.Parameter.Select(GetParameterContent));
             subHeaders.AddRange(
-                operation.Parameters.Items.Select(valmod => GetValidationModificationContent(valmod,
-                    stackLevel + 1))
-                );
-
+                operation.Parameters.Items.Select(GetValidationModificationContent));
+            subHeaders.ForEach(subHeader => header.AddSubHeader(subHeader));
             return header;
         }
 
-        private static HeaderType GetValidationModificationContent(object validationModification, int stackLevel)
+        private static HeaderType GetValidationModificationContent(object validationModification)
         {
             ValidationType validation = validationModification as ValidationType;
             ModificationType modification = validationModification as ModificationType;
             if (validation != null)
-                return GetValidationContent(validation, stackLevel);
+                return GetValidationContent(validation);
             else if (modification != null)
-                return GetModificationContent(modification, stackLevel);
+                return GetModificationContent(modification);
             else
                 throw new NotSupportedException("ValidationModification type: " + validationModification.GetType().Name);
         }
 
-        private static HeaderType GetModificationContent(ModificationType modification, int stackLevel)
+        private static HeaderType GetModificationContent(ModificationType modification)
         {
-            HeaderType header = new HeaderType();
-            header.text = "Modification: " + modification.name;
-            header.level = stackLevel;
+            HeaderType header = new HeaderType {text = "Modification: " + modification.name};
             string styleName = modification.state.ToString();
             header.SetHeaderTextContent(styleName, modification.designDesc);
             foreach (var target in modification.Target ?? new TargetType[0])
@@ -77,11 +73,9 @@ namespace OperationToDocumentationTRANS
             return header;
         }
 
-        private static HeaderType GetValidationContent(ValidationType validation, int stackLevel)
+        private static HeaderType GetValidationContent(ValidationType validation)
         {
-            HeaderType header = new HeaderType();
-            header.text = "Validation: " + validation.name;
-            header.level = stackLevel;
+            HeaderType header = new HeaderType {text = "Validation: " + validation.name};
             string styleName = validation.state.ToString();
             header.SetHeaderTextContent(styleName, validation.designDesc);
             foreach (var target in validation.Target ?? new TargetType[0])
@@ -89,12 +83,11 @@ namespace OperationToDocumentationTRANS
             return header;
         }
 
-        private static HeaderType GetParameterContent(VariableType parameter, int stackLevel)
+        private static HeaderType GetParameterContent(VariableType parameter)
         {
             HeaderType header = new HeaderType
                                     {
                                         text = parameter.name + " (" + parameter.dataType + ")",
-                                        level = stackLevel,
                                     };
             header.SetHeaderTextContent(parameter.state.ToString(), parameter.designDesc);
             return header;
